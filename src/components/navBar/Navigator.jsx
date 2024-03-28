ff898154285e207cc6aef32c2909f37cfccf9519
Julian Martin <julianmartin19@outlook.com>
2024-03-28 20:03:08 -0300

    Correccion clase css

--- src/components/navBar/Navigator.jsx
+++ src/components/navBar/Navigator.jsx
@@ -6,31 +6,31 @@ import '../navBar/Navigator.css'
 import imgCarrito from '../../assets/img/imgCarrito.png'
 
 
-export function Navigator () {
+export function Navigator() {
   return (
     <>
-     <div className='ms-auto text-center colorDivTop' xs={12}><img src={imgCarrito} alt="Carrito_logo" />RollingMarket <img src={imgCarrito} alt="Carrito_logo" /></div>
-{/* probar el cambio de color en el div */}
+      <div className='ms-auto text-center colorDivTopNav' xs={12}><img src={imgCarrito} alt="Carrito_logo" />RollingMarket <img src={imgCarrito} alt="Carrito_logo" /></div>
+      {/* probar el cambio de color en el div */}
 
-    <Navbar expand="lg" className="bg-body-secondary p-2">
-      <Container fluid>
-        <Navbar.Brand href="#home"><Image src =""/></Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav>
-            <Nav.Link href="#home">LOGO</Nav.Link>
-            <Nav.Link href="#link">Contacto</Nav.Link>
-            <Nav.Link href="#link">Quienes Somos</Nav.Link>
-           </Nav>
-          <div className='p-2 ms-auto'>
-            <Navbar.Text>
-            Bienvenido <a href="#login">PANEL DE USUARIO O ADMIN</a>
-            {/* {user ? "Bienvenido" + user.nombre : "NO ESTAS LOGEADO"} */}
+      <Navbar expand="lg" className="bg-body-secondary p-2">
+        <Container fluid>
+          <Navbar.Brand href="#home"><Image src="" /></Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav>
+              <Nav.Link href="#home">LOGO</Nav.Link>
+              <Nav.Link href="#link">Contacto</Nav.Link>
+              <Nav.Link href="#link">Quienes Somos</Nav.Link>
+            </Nav>
+            <div className='p-2 ms-auto'>
+              <Navbar.Text>
+                Bienvenido <a href="#login">PANEL DE USUARIO O ADMIN</a>
+                {/* {user ? "Bienvenido" + user.nombre : "NO ESTAS LOGEADO"} */}
 
-          </Navbar.Text>
-          </div>
-          <Nav className='p-2 ms-auto'>
-            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              </Navbar.Text>
+            </div>
+            <Nav className='p-2 ms-auto'>
+              {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
@@ -41,11 +41,11 @@ export function Navigator () {
                 Separated link
               </NavDropdown.Item>
             </NavDropdown> */}
-            <Button className='p-2 ms-auto' variant="success">Iniciar Sesión</Button>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+              <Button className='p-2 ms-auto' variant="success">Iniciar Sesión</Button>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
   );
 };
