aa518554fafcdbf9d746b990f2fe5a99bad2e94b
AvalosJorge <xjorgeluis93x@gmail.com>
2024-03-29 19:19:59 -0300

    corrección de componente footer, mayuscula.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Footer from "./components/footer/Footer";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -30,7 +30,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
 
-    
+    <Footer/>
     </>
   )
 }
