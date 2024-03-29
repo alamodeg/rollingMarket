9f0eb236f681454235418fd8760140cc09c29238
AvalosJorge <xjorgeluis93x@gmail.com>
2024-03-29 19:11:53 -0300

    Creacion estructura footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -28,6 +29,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+    
     </>
   )
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,10 @@
+
+const footer = () => {
+  return (
+    <>
+      <h1>Este es el footer</h1>
+    </>
+  )
+}
+
+export default footer
