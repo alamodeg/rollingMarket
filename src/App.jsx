06380d792c37cf2fffa7ad995f80c44871388f8d
Alvaro Deguer <alvarodeguer@gmail.com>
2024-03-31 17:35:05 -0300

    Importado el componente MainPage para renderizar la App

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import viteLogo from '/vite.svg'
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css'
 import { Navigator } from './components/navBar/Navigator.jsx'
+import MainPage from './components/mainPage/MainPage.jsx';
 
 function App() {
   return (
     <>
     <Navigator/>
+    <MainPage/>
 
 
     </>
