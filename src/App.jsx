b9bbe6c6683a0627bff3efc04705750613ff71ee
AvalosJorge <xjorgeluis93x@gmail.com>
2024-04-02 12:17:53 -0300

    Creacion y estructuración de página error

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css'
-import "bootstrap/dist/css/bootstrap.min.css";
 import Footer from "./components/footer/Footer";
 import { Navigator } from './components/navBar/Navigator.jsx'
+import Page404 from './page/404/Page404';
 
 function App() {
   return (
     <>
-     
-     <Navigator/>
+      <Navigator/>
+      <Page404/>
       <Footer />
     </>
   );
---
+++ src/components/404/Error404.jsx
@@ -0,0 +1,11 @@
+import './styleError404.css'
+
+const Error404 = () => {
+  return (
+    <>
+      <h1>error</h1>
+    </>
+  )
+}
+
+export default Error404
