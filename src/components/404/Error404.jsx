a34b10e1ee9be583957a6e745d9bc01b4bee69bd
AvalosJorge <xjorgeluis93x@gmail.com>
2024-04-05 22:14:17 -0300

    Modificación de colores

--- src/components/404/Error404.jsx
+++ src/components/404/Error404.jsx
@@ -1,23 +1,25 @@
-import './styleError404.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import error404 from '../../assets/img/imgError404/error404.png';
 import Button from 'react-bootstrap/Button';
+import './styleError404.css';
 
 const Error404 = () => {
   return (
-    <Container fluid className='imgBack d-flex justify-content-center align-items-center'>
-      <main>
+    <Container fluid className='imgBack justify-content-center align-items-center'>
+      <Container>
         <Row className='justify-content-center align-items-center'>
           <div className='col-12 col-md-6 text-center'>
             <img src={error404} className='imgShoppingCart' alt="imagenError404" />
           </div>
-          <div className='col-12 col-md-6 text-center'>
-            <h1 className='tittleError'>Ups! <strong>404</strong>: Página no encontrada</h1>
-            <Button className='buttomBack'>Volver a la página principal</Button>{' '}
+          <div className='col-12 col-md-6 text-center mt-2'>
+            <h1 className='tittleError'><strong>¡Error 404!</strong>
+            <br />
+             Página no encontrada</h1>
+            <Button variant='link' className='buttomBack'>Volver a la página principal</Button>{' '}
           </div>
         </Row>
-      </main>
+      </Container>
     </Container>
   );
 }
