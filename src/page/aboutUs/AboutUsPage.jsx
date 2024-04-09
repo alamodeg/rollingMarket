724e3402a0b8262be56087db62cae1c0e2389c3a
Gabriel Urueña <gabrieluruena01@gmail.com>
2024-04-09 19:36:22 -0300

    creacion de page

---
+++ src/page/aboutUs/AboutUsPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { AboutUs } from '../../components/aboutUs/AboutUs'
+
+const AboutUsPage = () => {
+  return (
+    <AboutUs></AboutUs>
+  )
+}
+
+export default AboutUsPage
