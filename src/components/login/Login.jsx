1360fb147c1b3088e147f5fd828df6c668ff151a
Gabriel Urueña <gabrieluruena01@gmail.com>
2024-04-08 22:28:25 -0300

    estructurción de carpetas, y desarrollo del login

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -5,35 +5,44 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const Login = () => {
   return (
-    <Container fluid>
-        <Row>
-            <Col>
-            <Card>
-            <Form className="cardLogin">
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+    <body className="bodyLogin"> 
+      <Container fluid className="containerLogin">
+        <Row className="justify-content-center">
+          <Col className="d-flex justify-content-center colLogin" md={12} > 
+            <Card className="cardLoginPrincipal align-items-center">
+              <Form className="formLogin d-flex d-flex flex-column">
+              <div className="opcionContraseñaLogin">
+                <p>¿Aún no tenes una cuenta? <a className="etiquetaLogin"href="#" >Solicitá tu alta de usuario</a></p>
+              </div>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>Ingresá tu email</Form.Label>
+                  <Form.Control type="email" placeholder="Email" />
+                  <Form.Text className="text-muted">
+                  </Form.Text>
+                </Form.Group>
+  
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                  <Form.Label>Contraseña</Form.Label>
+                  <Form.Control type="password" placeholder="Contraseña" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                  <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="primary" className="botonFormLogin mb-4" type="submit">
+                  Ingresar
+                </Button>
+              <div className="opcionRegistro">
+                <p>¿Olvidaste tu contraseña? <a className="etiquetaLogin"href="#" >Recupera tu cuenta</a></p>
+              </div>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
-    </Card>
-            </Col>
+              </Form>
+             
+            </Card>
+          </Col>
         </Row>
-    </Container>
-  )
+      </Container>
+    </body>
+  ) 
 }
 
 export default Login
