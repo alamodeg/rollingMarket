d069c44a6e17113539d78425c18ab50898171f49
Julian Martin <julianmartin19@outlook.com>
2024-04-11 19:08:18 -0300

    cambios en app previo a merge

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css'
 import { Navigator } from './components/navBar/Navigator.jsx'
 import Footer from './components/footer/Footer.jsx';
-import { Contact } from './page/contacto/Contact.jsx';
 
 
 function App() {
   return (
     <>
     <Navigator/>
-    <Contact/>
+
     <Footer/>
     </>
   );
