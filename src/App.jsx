4ba52f37a35b2881e6977c4099d783f7d10065bd
Julian Martin <julianmartin19@outlook.com>
2024-04-11 19:21:27 -0300

    cambios en apps

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css'
-<<<<<<< HEAD
-import { Navigator } from './components/navBar/Navigator.jsx'
-=======
 import "bootstrap/dist/css/bootstrap.min.css";
 import Footer from "./components/footer/Footer";
->>>>>>> 3bf509ec6a0456c4b20d843b612c76585f5b96ae
+import { Navigator } from './components/navBar/Navigator';
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
-    <Navigator/>
-
-
-=======
-     
+      <Navigator/>
 
       <Footer />
->>>>>>> 3bf509ec6a0456c4b20d843b612c76585f5b96ae
     </>
   );
 }
