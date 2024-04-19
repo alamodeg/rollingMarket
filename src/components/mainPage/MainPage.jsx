5e9885b595491fe1f581a475e819b6ae209cf387
Alvaro D. <alvarodeguer@gmail.com>
2024-04-18 23:28:08 -0300

    Probando hooks para traer datos desde API con axios

--- src/components/mainPage/MainPage.jsx
+++ src/components/mainPage/MainPage.jsx
@@ -1,119 +1,33 @@
-import { useMemo } from 'react';
-import {
-  MaterialReactTable,
-  useMaterialReactTable,
-} from 'material-react-table';
+import React from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 
-//nested data is ok, see accessorKeys in ColumnDef below
-const data = [
-  {
-    product: 'Leche Nido',
-    description: 'Leche descremada larga vida La Serenísima cero lactosa 1L adicionada con oligofructosa e insulina. Libre de gluten. sin TAC',
-    stock: 2,
-    modifiedDate: '15-03-2004',
-  },
-  {
-    product: ' Gaseosa Coca Cola',
-    description: 'Coca-cola Zero 2,25 Lt',
-    stock: 15,
-    modifiedDate: '15-03-2005',
-  },
-  {
-    product: 'Martillo Galponero',
-    description: 'Martillo Galponero "Mc Carthy"',
-    stock: 37,
-    modifiedDate: '01-01-2001',
-  },
-  {
-    product: 'Silla de Oficina',
-    description: 'Silla De Oficina "Turin" 50x52x97 cm',
-    stock: 18,
-    modifiedDate: '05-07-2015',
-  },
-  {
-    product: 'Libro "La Saga De Los Confines"',
-    description: 'Editorial PHL, en español con 480 hojas a color',
-    stock: 17,
-    modifiedDate: '20-08-2008',
-  },
-  {
-    product: 'Alimento para perros "Pedigree"',
-    description: 'Pedigree 15 kg para adultos carne, pollo y cereales',
-    stock: 0,
-    modifiedDate: '14-03-2024',
-  },
-];
 
 const MainPage = () => {
-  //should be memoized or stable
-  const columns = useMemo(
-    () => [
-      {
-        accessorKey: 'product', //normal accessorKey
-        header: 'Productos',
-        size: 150,
-      },
-      {
-        accessorKey: 'description',
-        header: 'Descripcion',
-        size: 150,
-      },
-      {
-        accessorKey: 'stock',
-        header: 'Stock',
-        size: 50,
-        enableColumnFilter: false,
-        muiTableHeadCellProps: {
-            align: 'center',
-          },
-          muiTableBodyCellProps: {
-            align: 'center',
-          },
-      },
-      {
-        accessorKey: 'categoria',
-        header: 'Categoria',
-        size: 150,
-        filterSelectOptions: ['Bebidas', 'Limpieza', 'Herramientras & Hogar', 'Mascotas','Muebles','Entretenimiento'],
-        filterVariant: 'multi-select'
 
-        
-      },
-      {
-        accessorKey: 'modifiedDate',
-        header: 'Fecha Modificacion:',
-        size: 150,
+  const [users, setUsers] = useState([])
+  
+  
+  
+  useEffect(() => { // Aquí comenzamos el bloque de efecto
+    const obtenerDatos = async () => { // Definimos la función para obtener datos
+      try {
+        const response = await axios.get("http://localhost:8000/users");
+        //setUsers(response.data)
+      } catch (error) {
+        console.error('Error en GET:', error); // Manejamos cualquier error de solicitud
+      }
+    };
+    obtenerDatos();
 
-      },
-    ],
-    [],
+  }, []);
+
+  console.log(users, "usuarios")
+
+  return (
+    <div>Soy una tabla</div> // Retornamos el JSX del componente
   );
-
-  const table = useMaterialReactTable({
-    columns,
-    data, //data must be memoized or stable (useState, useMemo, defined outside of this component, etc.)
-    enableColumnOrdering : true,
-    enableRowSelection: true,
-    initialState: { showColumnFilters: false },
-
-    muiFilterTextFieldProps: {
-
-      sx: { m: '0.5rem 0', width: '100%' },
-
-      variant: 'outlined',
-
-    },
-
-    //align header & cells
-    muiTableHeadCellProps: {align: 'center', },
-    muiTableBodyCellProps: {align: 'center', },
-
-    enableColumnActions: false,
-    
-  });
-
-  return <MaterialReactTable table={table} />;
 };
 
-export default MainPage;
+export default MainPage; // Exportamos el componente MainPage
