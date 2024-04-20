66f60211c0e525d3688ea64eb4ae1ddbae023df9
Julian Martin <julianmartin19@outlook.com>
2024-04-20 19:45:48 -0300

    test login

--- src/context/UsersContext.jsx
+++ src/context/UsersContext.jsx
@@ -6,10 +6,13 @@ export const UsersProvider = createContext();
 // eslint-disable-next-line react/prop-types
 const UsersContext = ({ children }) => {
   const [usuarios, setUsuarios] = useState([]);
+  const [usuarioLogueado, setUsuarioLogueado] = useState();
+
 
   const getUsers = async () => {
     try {
-      const response = await axios.get("http://localhost:8000/users");
+      const response = await axios.get(
+        "https://rollingmarketbe.onrender.com/usuarios");
       setUsuarios(response.data);
     } catch (error) {
       console.log(error);
@@ -18,7 +21,7 @@ const UsersContext = ({ children }) => {
 
   const addUser = async (usuario) => {
     try {
-      await axios.post("http://localhost:8000/users", usuario);
+      await axios.post("https://rollingmarketbe.onrender.com/usuario", usuario);
       await getUsers(); //actualizar la lista de usuarios
     } catch (error) {
       console.log(error);
@@ -27,7 +30,7 @@ const UsersContext = ({ children }) => {
 
   const deleteUsuario = async (id) => {
     try {
-      await axios.delete(`http://localhost:8000/users/${id}`);
+      await axios.delete(`https://rollingmarketbe.onrender.com/usuario/${id}`);
       await getUsers();
     } catch (error) {
       console.log(error);
@@ -36,7 +39,7 @@ const UsersContext = ({ children }) => {
 
   const editUsuario = async (usuario) => {
     try {
-      await axios.put(`http://localhost:8000/users/${usuario.id}`, usuario);
+      await axios.put(`https://rollingmarketbe.onrender.com/usuario/delete/${usuario.id}`, usuario);
       await getUsers();
     } catch (error) {
       console.log(error);
@@ -48,34 +51,34 @@ const UsersContext = ({ children }) => {
     window.location.href = "/";
   };
 
+  const loginUsuario = async (usuario) => {
+    console.log(usuario, "loginUsuario");
+    try {
+      const response = await axios.post(
+        "https://rollingmarketbe.onrender.com/login/",
+        usuario
+      );
+      console.log(response.data, "response.data desde el context");
+
+      const { token } = response.data.data;
+      localStorage.setItem("token", token);
+      const decoded = jwtDecode(token);
+
+      setUsuarioLogueado(decoded);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
     getUsers();
   }, []);
 
   return (
-    <UsersProvider.Provider value={{ usuarios, getUsers, addUser, logOut, deleteUsuario, editUsuario }}>
+    <UsersProvider.Provider value={{ usuarios, loginUsuario, getUsers, addUser, logOut, deleteUsuario, editUsuario, usuarioLogueado }}>
       {children}
     </UsersProvider.Provider>
   );
 };
-
-
-// const [users, setUsers] = useState([])
-
-//       useEffect(() => {
-//         const obtenerDatos = async () => {
-//           try {
-//             const url = 'https://jsonplaceholder.typicode.com/todos/'
-//             const response = await fetch(url)
-//             const resultado = await response.json()
-//             console.log(resultado)
-//             setUsers(response.data);
-//           }
-//           catch (error) {
-//             console.error(error);
-//           }
-//         }
-//         obtenerDatos();
-//       }, []);
 
 export default UsersContext;
