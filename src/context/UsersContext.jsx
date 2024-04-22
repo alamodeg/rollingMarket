6c160398bda8cd32e89455a3827ff4f1734ca01e
AvalosJorge <xjorgeluis93x@gmail.com>
2024-04-21 21:38:13 -0300

    text login

--- src/context/UsersContext.jsx
+++ src/context/UsersContext.jsx
@@ -1,81 +1,43 @@
-import { createContext, useEffect, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import axios from "axios";
+import { jwtDecode } from "jwt-decode";
 
 export const UsersProvider = createContext();
 
-// eslint-disable-next-line react/prop-types
 const UsersContext = ({ children }) => {
-  const [usuarios, setUsuarios] = useState([]);
+  const [usuarioLogueado, setUsuarioLogueado] = useState();
 
-  const getUsers = async () => {
+  const loginUsuario = async (usuario) => {
     try {
-      const response = await axios.get("http://localhost:8000/users");
-      setUsuarios(response.data);
+      const response = await axios.post(
+        "http://localhost:4000/login/",
+        usuario
+      );
+      const { token } = response.data.data;
+      localStorage.setItem("token", token);
+      const decoded = jwtDecode(token);
+      if (decoded) {
+        setUsuarioLogueado(decoded);
+      }
     } catch (error) {
       console.log(error);
+      throw new Error("Usuario o contraseña incorrectos");
     }
-  };
-
-  const addUser = async (usuario) => {
-    try {
-      await axios.post("http://localhost:8000/users", usuario);
-      await getUsers(); //actualizar la lista de usuarios
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const deleteUsuario = async (id) => {
-    try {
-      await axios.delete(`http://localhost:8000/users/${id}`);
-      await getUsers();
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const editUsuario = async (usuario) => {
-    try {
-      await axios.put(`http://localhost:8000/users/${usuario.id}`, usuario);
-      await getUsers();
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  const logOut = () => {
-    localStorage.removeItem("user");
-    window.location.href = "/";
   };
 
   useEffect(() => {
-    getUsers();
+    const token = localStorage.getItem("token");
+    if (token) {
+      const decoded = jwtDecode(token);
+      setUsuarioLogueado(decoded);
+    }
   }, []);
 
   return (
-    <UsersProvider.Provider value={{ usuarios, getUsers, addUser, logOut, deleteUsuario, editUsuario }}>
+    <UsersProvider.Provider value={{ loginUsuario, usuarioLogueado }}>
       {children}
     </UsersProvider.Provider>
   );
 };
 
-
-// const [users, setUsers] = useState([])
-
-//       useEffect(() => {
-//         const obtenerDatos = async () => {
-//           try {
-//             const url = 'https://jsonplaceholder.typicode.com/todos/'
-//             const response = await fetch(url)
-//             const resultado = await response.json()
-//             console.log(resultado)
-//             setUsers(response.data);
-//           }
-//           catch (error) {
-//             console.error(error);
-//           }
-//         }
-//         obtenerDatos();
-//       }, []);
-
-export default UsersContext;
+export default UsersContext;
