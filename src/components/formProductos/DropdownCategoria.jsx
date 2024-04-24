26b70b084f041691814fb9dcb757f4eb11d68935
Alvaro D. <alvarodeguer@gmail.com>
2024-04-24 11:37:30 -0300

    Agregado dropdown de categorias

---
+++ src/components/formProductos/DropdownCategoria.jsx
@@ -0,0 +1,38 @@
+import Dropdown from 'react-bootstrap/Dropdown';
+import { useContext,useState } from 'react';
+import { ProductsProvider } from '../../context/ProductosContext';
+
+function DropdownCategoria() {
+  const { getCategoria } = useContext(ProductsProvider);
+  
+  const handleCategorySelect = (categoria) => {
+    getCategoria(categoria);
+  };
+
+  return (
+    <Dropdown>
+      <Dropdown.Toggle variant="primary" id="dropdown-basic">
+        Buscar por Categoría
+      </Dropdown.Toggle>
+      <Dropdown.Menu>
+        <Dropdown.Item onClick={() => handleCategorySelect('Bebidas')}>
+          Bebidas
+        </Dropdown.Item>
+        <Dropdown.Item onClick={() => handleCategorySelect('Golosinas')}>
+          Golosinas
+        </Dropdown.Item>
+        <Dropdown.Item onClick={() => handleCategorySelect('Limpieza')}>
+          Limpieza
+        </Dropdown.Item>
+        <Dropdown.Item onClick={() => handleCategorySelect('Lacteos')}>
+          Lácteos
+        </Dropdown.Item>
+        <Dropdown.Item onClick={() => handleCategorySelect('Otros')}>
+          Otros
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+}
+
+export default DropdownCategoria;
